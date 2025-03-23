9debeae7a2b2433ca07666244fedfe06af0fe70d
Gabsolrx <omoleyeolusola1@gmail.com>
2025-03-23 22:15:22 +0100

    python_week2_as

---
+++ Week2.py
@@ -0,0 +1,28 @@
+#To create an empty list
+my_list = []
+
+#To append 
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+#To insert 15
+my_list.insert(1, 15)
+
+#To extend the list
+my_list.extend([50, 60, 70])
+
+#To remove the last number
+my_list.pop()
+
+#To sort in ascending order
+my_list.sort()
+
+#To find and print the index of the value 30
+index_of_30 = my_list.index(30)
+print("Index of 30:", index_of_30)
+
+
+
+
